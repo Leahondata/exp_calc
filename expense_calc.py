ca4a1cc6bf308eb54fde2e4c50cb943bf4d48983
Leahondata <8v99c8j5@duck.com>
2023-02-05 16:54:57 +0200

    V1.1, Optimized csv data check and menu file loading

--- expense_calc.py
+++ expense_calc.py
@@ -17,10 +17,11 @@ def empty_csv(filename):
     with open(filename, mode='r') as file:
         csv_data = csv.reader(file)
         data = list(csv_data)
-        if bool(data) == False:
+        if not data:
             print('Please enter an expense first!')
+            return False
         else:
-            return
+            return data
 
 
 def read_csv(filename):
@@ -82,7 +83,6 @@ def get_input():
 
 def summarize(list_data):
     summary_dict = {}
-    print(list_data)
     for data in list_data:
         if data[1] not in summary_dict:
             summary_dict[data[1]] = float(data[2])
@@ -115,7 +115,7 @@ menu_state = 5
 while menu_state != 0:
     datafile_name = "expense_calc.csv"
     # print(read_csv(datafile_name))
-    print(f"\n=========[ExpCalc v1.0]==========")
+    print(f"\n=========[ExpCalc v1.1]==========")
     print("1. Add expense")
     print("2. Display expenses")
     print("3. Summarize spending")
@@ -127,21 +127,21 @@ while menu_state != 0:
     menu_state = int(input("Choice: "))
     os.system("clear")
     print("")
+    csv_data = empty_csv(datafile_name)
     if (menu_state == 1):
         user_data = get_input()
         write_csv(user_data, datafile_name)
     elif (menu_state == 2):
-        empty_csv(datafile_name)
-        for item in read_csv(datafile_name):
-            print(item)
+        if csv_data:
+            for item in read_csv(datafile_name):
+                print(item)
     elif (menu_state == 3):
-        empty_csv(datafile_name)
-        csv_data = read_csv(datafile_name)
-        for category, cost in summarize(csv_data).items():
-            print(category, cost)
+        if csv_data:
+            for category, cost in summarize(csv_data).items():
+                print(category, cost)
     elif (menu_state == 4):
-        empty_csv(datafile_name)
-        display_plot()
+        if csv_data:
+            display_plot()
 
 
 # user_data = get_input()
@@ -151,4 +151,3 @@ while menu_state != 0:
 # datafile_name = "expense_calc.csv"
 # csv_data = read_csv(datafile_name)
 # print(summarize(csv_data))Steam
-
